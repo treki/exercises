4f54e44ab2697cb06606062e4bceaf5ea1f03100
Tuya <tuya.senff@gmail.com>
2016-03-07 09:41:17 -0800

    added more info

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,33 @@
 var main = function(){
-	"use strict"
+	"use strict";
+var toDos = [
+"Finish writing this book", "Take Gracie to the park", "Answer emails",
+"Prep for Monday's class", "Make up some new ToDos", "Get Groceries"
+];
+        //... other tab-related stuff
+};
+	
 
-	console.log("hello world");
+$(".tabs a span").toArray().forEach(function (element) { 
+// create a click handler for this element 
+$(element).on("click", function () {
+// since we're using the jQuery version of element, 
+// we'll go ahead and create a temporary variable 
+// so we don't need to keep recreating it
+var $element = $(element);
+        $(".tabs a span").removeClass("active");
+        $element.addClass("active");
+        $("main .content").empty();
+if ($element.parent().is(":nth-child(1)")) { 
+	console.log("FIRST TAB CLICKED!");
+} else if ($element.parent().is(":nth-child(2)")) { 
+	console.log("SECOND TAB CLICKED!");
+} else if ($element.parent().is(":nth-child(3)")) {
+	 console.log("THIRD TAB CLICKED!");
+}
+return false;
+});
+});  
 };
 
 $(document).ready(main);
